03cda0229b43ffa0226dd38ed83e6297d5883680
Panicwave <72047743+patrick-media@users.noreply.github.com>
2020-12-23 22:01:39 -0600

    Update 5OUuwily6OTz7SerXUKm.js

--- cdn/js/5OUuwily6OTz7SerXUKm.js
+++ cdn/js/5OUuwily6OTz7SerXUKm.js
@@ -1,16 +1,24 @@
+var dd1vis = 0;
+var dd2vis = 0;
 function dd1show() {
+  dd1vis = 1;
+  if (dd2vis == 1) {
+    document.getElementById('dd2').style.display = 'none';
+  }
   document.getElementById('dd1').style.display = 'block';
 }
 function dd2show() {
+  dd2vis = 1;
+  if (dd1vis == 1) {
+    document.getElementById('dd1').style.display = 'none';
+  }
   document.getElementById('dd2').style.display = 'block';
 }
-
-function dd1_hideothers() {
+function dd_hideall() {
+  document.getElementById('dd1').style.display = 'none';
   document.getElementById('dd2').style.display = 'none';
 }
-function dd2_hideothers() {
-  document.getElementById('dd1').style.display = 'none';
-}
+
 /*
 var dd1 = document.getElementById('dd1');
 var dd2 = document.getElementById('dd2');
