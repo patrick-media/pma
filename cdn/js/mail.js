460b043d01d6a3f4982c9c9fa98f9116647d1f80
Panicwave <72047743+patrick-media@users.noreply.github.com>
2021-01-03 18:23:30 -0600

    Create mail.js

---
+++ cdn/js/mail.js
@@ -0,0 +1,9 @@
+function goto_inbox() {
+  document.title = "(1) Inbox - PMAil";
+}
+function goto_drafts() {
+  document.title = "Drafts - PMAil";
+}
+function goto_betainfo() {
+  document.title = "Beta - PMAil";
+}
