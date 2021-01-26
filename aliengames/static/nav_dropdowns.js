d85a68190fe39737ce8df099475e89cf15dbde3e
Panicwave <72047743+patrick-media@users.noreply.github.com>
2021-01-25 22:29:07 -0600

    Create nav_dropdowns.js

---
+++ aliengames/static/nav_dropdowns.js
@@ -0,0 +1,8 @@
+function ddshow_community() {
+  document.getElementById('dd_community').style.display = 'inline-table';
+  document.getElementById('btn_community_td').classList.add('nav-hi');
+}
+function dd_hideall() {
+  document.getElementById('dd_community').style.display = 'none';
+  document.getElementById('btn_community_td').classList.remove('nav-hi');
+}
